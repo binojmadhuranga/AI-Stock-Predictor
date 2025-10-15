657f31a87c5f0f8d1d08bed9808222a48ce455c5
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-10-15 16:20:28 +0530

    set POLYGON_API_KEY for local development

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Set your Polygon.io API key here for local development.
 // Get a key from: https://polygon.io/
 // Example: export const POLYGON_API_KEY = 'YOUR_REAL_KEY_HERE';
-export const POLYGON_API_KEY = '';
+export const POLYGON_API_KEY = '_8PLPXkgl5c2uEj7g7EdPXdpVLr2ZQOA';
